b963b07375ed519c6f2e655ac7b06bd19cf8ec4e
Kushagra Srivastava <srivastavakushagra2005@gmail.com>
2026-09-13 18:41:51 +0530

    Split booking route into /checkout (create order) and /verify (confirm payment)

--- routes/booking.js
+++ routes/booking.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const { isLoggedIn, validateBooking } = require("../middleware.js");
+const { isLoggedIn } = require("../middleware.js");
 const bookingController = require("../controllers/bookings.js");
 
 router.post(
-  "/",
+  "/checkout",
   isLoggedIn,
-  validateBooking,
-  wrapAsync(bookingController.createBooking),
+  wrapAsync(bookingController.createCheckoutOrder),
+);
+
+router.post(
+  "/verify",
+  isLoggedIn,
+  wrapAsync(bookingController.verifyAndCreateBooking),
 );
 
 module.exports = router;
