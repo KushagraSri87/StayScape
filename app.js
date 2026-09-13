17801192404d69f3cffc7f9232d5092cf415f5d6
Kushagra Srivastava <srivastavakushagra2005@gmail.com>
2026-09-13 19:05:43 +0530

    Add express.json() middleware to parse JSON request bodies for Razorpay checkout

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 
