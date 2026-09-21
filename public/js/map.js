50b900c79285bb1256fb22a883379856f0d32e7c
Kushagra Srivastava <srivastavakushagra2005@gmail.com>
2026-09-21 11:24:56 +0530

    Swap OSM tiles for CARTO dark basemap - production-safe, no API key, matches theme

--- public/js/map.js
+++ public/js/map.js
@@ -5,10 +5,15 @@ const [lng, lat] = listing.geometry.coordinates;
 
 const map = L.map("map").setView([lat, lng], 9);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "&copy; OpenStreetMap contributors",
-  maxZoom: 19,
-}).addTo(map);
+L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+  {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: "abcd",
+    maxZoom: 20,
+  },
+).addTo(map);
 
 L.marker([lat, lng])
   .addTo(map)
